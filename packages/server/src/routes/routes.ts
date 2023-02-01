d59f3a008ffd530904adf24816a34a33cba0ad03
aubeOSG <caube@osg.ca>
2023-02-01 09:50:12 -0500

    update - heroku logging

--- packages/server/src/routes/routes.ts
+++ packages/server/src/routes/routes.ts
@@ -1,10 +1,13 @@
 import path from 'path';
+import fs from 'fs';
 import express from 'express';
 
 const staticPath = path.join(process.cwd(), '../client/dist');
 const appPath = path.join(staticPath, 'index.html');
 
 export const init = (app: express.Application) => {
+  console.log('staticPath', staticPath);
+  console.log('path exists', fs.existsSync(staticPath));
   app.use('/public', express.static(staticPath));
 
   app.get('/app', (req, res) => {
